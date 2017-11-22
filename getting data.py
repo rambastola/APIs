22beae525b38ebba637e16e4928bdbc7d05ed2fb
Ram Bastola <rambastola7@gmail.com>
2017-11-22 17:01:57 -0500

    Update getting data.py

--- getting data.py
+++ getting data.py
@@ -3,17 +3,14 @@ import requests, json
 def search_walmart(answer):
     """
     ask user for product's name then search for it in walmart.com.
-
     :param answer: information pulled from walmart.com
     :return: name of the product and price
     """
     encrypted = answer.content
     data = json.loads(encrypted)
-
     products = data["items"]
 
     for product in products:
-
         name = product["name"]
         price  = product["salePrice"]
         upc = product["upc"]
@@ -24,9 +21,7 @@ def search_walmart(answer):
 def main():
 
     user = input("Enter product name ")
-
     response = requests.get("http://api.walmartlabs.com/v1/search?apiKey=bvngau7xave9du9m5wzhfdtb&query="+ user)
-
     search_walmart(response)
 
 main()
